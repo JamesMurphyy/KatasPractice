403e40c48d6f54d0e6e04a8492cfc98f89c91b08
James Murphy <jamesm8228@gmail.com>
2022-05-24 15:04:05 -0400

    Add refactoring for Grad file

---
+++ kata10Grad.js
@@ -0,0 +1,13 @@
+const multiplicationTable = function(maxValue) {
+  let ans = ""
+  for (let i in maxValue) {
+    let table = ""
+    for (let j in maxValue) {
+      table += 
+    }
+  }
+};
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
