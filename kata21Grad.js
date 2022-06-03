83976a4e0bab21ffdf6f724c83197130448191d3
James Murphy <jamesm8228@gmail.com>
2022-06-03 12:08:54 -0400

    Add code for kata 21

--- kata21Grad.js
+++ kata21Grad.js
@@ -1,10 +1,26 @@
 let prompt = require("prompt-sync")();
-let attempts = 0
-let number = 42
+let attempts = 0;
+let answer = Math.ceil(Math.random() * 100);
+let guess;
 // code below (replace this example)
-let answer = prompt("Guess a number: ");
-if (answer > number) {
 
-  console.log("Too High!");
+while (guess !== answer) {
+  console.log("Guess a answer between 1-100:");
 
+  guess = prompt("> ");
+
+  if (guess > answer) {
+    attempts += 1;
+    console.log("Too High!");
+  } else if (guess < answer) {
+    attempts += 1;
+    console.log("Too Low!");
+  } else if (isNaN(guess)) {
+    attempts += 1;
+    console.log("Not a number! Try again!");
+  } else {
+    attempts += 1;
+    console.log(`You got it! You took ${attempts} attempts!`);
+    break
+  }
 }
