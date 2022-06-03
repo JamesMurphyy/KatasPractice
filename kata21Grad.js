bfbfe2ed46d1e481a157da1450b62be4720fbebe
James Murphy <jamesm8228@gmail.com>
2022-06-03 10:43:17 -0400

    Add code to begin to answer kata 21

--- kata21Grad.js
+++ kata21Grad.js
@@ -1,5 +1,10 @@
 let prompt = require("prompt-sync")();
-
+let attempts = 0
+let number = 42
 // code below (replace this example)
 let answer = prompt("Guess a number: ");
-console.log("You answered: " + answer);
+if (answer > number) {
+
+  console.log("Too High!");
+
+}
