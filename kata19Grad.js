1313ff73cd972af3f031bd6195c5762f89981363
James Murphy <jamesm8228@gmail.com>
2022-06-26 22:24:40 -0400

    Add code to check and see if queens position is a threat

--- kata19Grad.js
+++ kata19Grad.js
@@ -1,6 +1,6 @@
 const generateBoard = function (whiteQueen, blackQueen) {
   let chessBoard = [];
-
+  //make empty chessboard
   for (let i = 0; i < 8; i++) {
     let row = [];
     for (let j = 0; j < 8; j++) {
@@ -17,10 +17,31 @@ const generateBoard = function (whiteQueen, blackQueen) {
   return chessBoard;
 };
 
+const queenThreat = function (chessBoard) {
 
+  let queensPosition = [];
 
+  // find the queens position
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      if (chessBoard[i][j] === 1) queensPosition.push([i, j]);
+    }
+  }
+
+  //determine if queen position is a threat
+  if (queensPosition[0][0] === queensPosition[1][0] || queensPosition[0][1] === queensPosition[1][1]) {
+    return true;
+  } else if (Math.abs(queensPosition[0][0] - queensPosition[1][0]) === Math.abs(queensPosition[0][1] - queensPosition[1][1])) {
+    return true;
+  } else {
+    return false;
+  }
+
+};
+
+//should return true
 let whiteQueen = [0, 5];
 let blackQueen = [5, 0];
 let generatedBoard = generateBoard(whiteQueen, blackQueen);
 console.log(generatedBoard);
-// console.log(queenThreat(generatedBoard));
+console.log(queenThreat(generatedBoard));
