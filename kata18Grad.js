7a7d3765251950df86fa036f705754ab52ecd7cd
James Murphy <jamesm8228@gmail.com>
2022-06-26 22:02:15 -0400

    Add code for kata 18

--- kata18Grad.js
+++ kata18Grad.js
@@ -1,10 +1,12 @@
 const squareCode = message => {
   let string = message.replace(/\s+/g, "").split('');
   let length = Math.ceil(Math.sqrt(string.length));
-  console.log(length)
+  // console.log(length)
+  // console.log(string)
+  return string.reduce((acc, l, i) => (acc[i%length] += l, acc), new Array(length).fill('')).join(' ');
 };
 
-
+// , new Array(length).fill('')
 console.log(squareCode("chill out"));
 console.log(squareCode("feed the dog"));
 console.log(squareCode("have a nice day"));
