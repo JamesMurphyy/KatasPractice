61cb0fa453cbbaa8edd84d53d28b7f962e88e557
James Murphy <jamesm8228@gmail.com>
2022-07-11 12:08:59 -0400

    Add kataCodeWars examples and tests

--- kataCodeWars.js
+++ kataCodeWars.js
@@ -17,3 +17,35 @@ Given a year, return the century it is in.
 function century(year) {
   return Math.ceil(year/100); //using ceiling method to round up to nearest century (100)
 }
+
+//Practice 2
+/*
+
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+
+
+*/
+
+
+//ANSWER
+
+function abbrevName(name){
+  let in1 = ''
+  let in2 = ''
+  
+  in1 = name[0]
+  for (let i = 0; i <= name.length; i++) {
+    if (name[i] == " ") {
+      in2 = name[i + 1]
+    }
+  }
+  return `${in1.toUpperCase()}.${in2.toUpperCase()}`
+}
