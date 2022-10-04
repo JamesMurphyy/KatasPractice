3f7ff1f2e0d858ed588ff50aafc43d0907a4c7c1
James Murphy <jamesm8228@gmail.com>
2022-10-04 13:49:39 -0400

    Add reverse string kata

--- KATAS2/reversingString.js
+++ KATAS2/reversingString.js
@@ -1,8 +1,16 @@
-function reverse() {
-
-
+function reverse(text) {
+  let reversedWords = "";
+  for (let x = text.length - 1; x >= 0; x--) {
+    reversedWords += text[x];
+    // console.log(reversedWords);
+  }
+  return reversedWords;
 }
 
+const args = process.argv.slice(2);
+for (let y = 0; y < args.length; y++) {
+  console.log(reverse(args[y]));
+}
 
 // const reverse = function() {
 
