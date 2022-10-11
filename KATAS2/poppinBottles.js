ef539caa99d47a538fdad0c6ae3cceb9f42cf8fc
James Murphy <jamesm8228@gmail.com>
2022-10-11 19:40:04 -0400

    Add first function for poppinBottles

--- KATAS2/poppinBottles.js
+++ KATAS2/poppinBottles.js
@@ -5,36 +5,42 @@ You've just been hired at Lighthouse Markets, a reputable (and thoroughly fictio
 For every two empty bottles, you can get one free (full) bottle of pop
 For every four bottle caps, you can get one free (full) bottle of pop
 Each bottle of pop costs $2 to purchase
+
 Instruction
 Given the above parameters, write a program so that you can figure out how many total bottles of pop can be redeemed given a customer investment.
 
 */
 
-
 // Task 1
 // Figure out the algorithm that will calculate this. For example, given a $20 investment, the customer will get 10 bottles of pop. That gives a supply of both bottles and bottle caps that can be used to redeem for further bottles, which will then produce a further supply for recycling.
 
+const bottles = function (investmentOfBottles) {
+  // let costPerBottle = 2;
+  // let costOfOrder = costPerBottle * numOfBottles;
+  // let profit = 0;
 
-
+  let numOfBottleCaps = 0;
+  let numOfEmptyBottles = 0;
+  let totalBottles = investmentOfBottles % 2;
+  if (totalBottles) {
+    for (let i = 0; i > numOfBottles; i++) {
+      numOfBottleCaps++;
+      numOfEmptyBottles++;
+    }
+    if (numOfBottleCaps % 4 === 0) {
+      totalBottles++;
+    }
+    if (numOfEmptyBottles % 2 === 0) {
+      totalBottles++;
+    }
+  }
+};
 
 // Task 2
 // Have your program accept an argument (argv) that will allow the user to specify the amount (in dollars) that the customer is going to spend. The node script will then calculate the total number of bottles that the customer will receive.
 
-
-
-
-
 // Task 3
 // Enhance the output of your program so that once the amount has been given, it provides a breakdown of how many bottles were purchased, how many were obtained through bottle recycling, and how many were obtained through bottle cap recycling.
 
-
-
-
 // Task 4
 // Add to the output, so that the program will tell the customer how many bottles and bottle caps they will have left over. We have to upsell the customer on buying more pop after all!
-
-
-
-
-
-
