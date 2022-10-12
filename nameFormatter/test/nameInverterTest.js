63e817670b27c5fb261a48568e7277cafc141d4a
James Murphy <jamesm8228@gmail.com>
2022-10-12 19:56:54 -0400

    Add test features needed to pass

--- nameFormatter/test/nameInverterTest.js
+++ nameFormatter/test/nameInverterTest.js
@@ -1,8 +1,72 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const nameInverter = require('../nameInverter.js');
+const nameInverter = require("../nameInverter.js");
 
-describe('nameInverter', function() {
-
+describe("nameInverter", function () {
+  it("return an empty string when passed an empty string", function () {
+    input: "";
+    output: "";
+  });
+  it("return a single name when passed a single name", function () {
+    input: "name";
+    output: "name";
+  });
+  it("return a single name when passed a single name with extra spaces", function () {
+    input: " name ";
+    output: "name";
+  });
+  it("return a last-name, first-name when passed a first and last-name", function () {
+    input: "first last";
+    output: "last, first";
+  });
+  it("Stretch: return a last-name, first-name when passed a first and last-name with extra spaces around the names (We may want to use regular expressions to detect the whitespace)", function () {
+    input: " first last";
+    output: "last, first";
+  });
+  it("return an empty string when passed a single honorific", function () {
+    input: "Dr. ";
+    output: "";
+  });
+  it("return honorific first-name when passed honorific first-name", function () {
+    input: "Dr. first";
+    output: "Dr. first";
+  });
+  it("return a honorific last-name, first-name when passed honorific first-name last-name", function () {
+    input: "Dr. first-name last-name";
+    output: "Dr. last-name, first-name";
+  });
+  it("return a honorific last-name, first-name when passed honorific first-name last-name with extra spaces around the words", function () {
+    input: " Dr. first-name last-name ";
+    output: "Dr. last-name, first-name";
+  });
+  it("throw an error when name is undefined", function () {
+    input: undefined;
+    output: throw "Error";
+  });
 });
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
