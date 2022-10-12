6c9689c62b45e9eb7414df505aab653d6794cd59
James Murphy <jamesm8228@gmail.com>
2022-10-11 20:10:23 -0400

    Add functionality for poppinBottles function to accept an argument (argv)

--- KATAS2/poppinBottles.js
+++ KATAS2/poppinBottles.js
@@ -15,15 +15,6 @@ Given the above parameters, write a program so that you can figure out how many 
 // Figure out the algorithm that will calculate this. For example, given a $20 investment, the customer will get 10 bottles of pop. That gives a supply of both bottles and bottle caps that can be used to redeem for further bottles, which will then produce a further supply for recycling.
 
 const bottles = function (investmentOfBottles) {
-  // let costPerBottle = 2;
-  // let costOfOrder = costPerBottle * numOfBottles;
-  // let profit = 0;
-
-  // let numOfBottleCaps = 0;
-  // let numOfEmptyBottles = 0;
-
-
-  
   let bottlesPurchased = Math.floor(investmentOfBottles / 2);
   let bottlesFromCaps = Math.floor(bottlesPurchased / 4);
   let bottlesFromEmpties = Math.floor(bottlesPurchased / 2);
@@ -33,11 +24,10 @@ const bottles = function (investmentOfBottles) {
   return totalBottles;
 };
 
-
-console.log(bottles(10));
-console.log(bottles(20));
-console.log(bottles(30));
-console.log(bottles(40));
+// console.log(bottles(10));
+// console.log(bottles(20));
+// console.log(bottles(30));
+// console.log(bottles(40));
 
 // for (let i = 0; i <= totalBottles; i++) {
 //   numOfBottleCaps++;
@@ -54,6 +44,11 @@ console.log(bottles(40));
 
 // Task 2
 // Have your program accept an argument (argv) that will allow the user to specify the amount (in dollars) that the customer is going to spend. The node script will then calculate the total number of bottles that the customer will receive.
+
+
+let result = bottles(process.argv.slice(2))
+console.log(result)
+
 
 // Task 3
 // Enhance the output of your program so that once the amount has been given, it provides a breakdown of how many bottles were purchased, how many were obtained through bottle recycling, and how many were obtained through bottle cap recycling.
