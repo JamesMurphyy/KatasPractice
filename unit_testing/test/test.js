884b492153b9833dcf8ecd952c3961e14758d15b
James Murphy <jamesm8228@gmail.com>
2022-10-12 18:49:45 -0400

    Add more test features

--- unit_testing/test/test.js
+++ unit_testing/test/test.js
@@ -6,4 +6,11 @@ describe("The function groupValidator()", () => {
     it("should return true if there are between 2 and 5 group members", () => {
       assert(validator.isGroupValid(["a", "b", "c"])).to.be.true
     });
+    //Not testing happy path
+    it("should return false when there are fewer than 2 group members", () => {
+      assert(validator.isGroupValid(["a"])).to.be.false
+    });
+    it("should return false when there are more than 5 group members", () => {
+      assert(validator.isGroupValid(["a", "b", "c", "d", "e", "f"])).to.be.false
+    });
 });
