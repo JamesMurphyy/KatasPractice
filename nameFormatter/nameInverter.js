cb8de71a1cb75dc29764320b47cf322aefa54c0a
James Murphy <jamesm8228@gmail.com>
2022-10-12 20:13:10 -0400

    Add function to make testing values correct

--- nameFormatter/nameInverter.js
+++ nameFormatter/nameInverter.js
@@ -1,5 +1,18 @@
-const nameInverter = function(name) {
+const nameSwitch = function (firstname, lastname) {
+  return `${lastname}, ${firstname}`;
+};
 
-}
+const nameInverter = function (name) {
+  name = name.trim();
+  if (name === "") {
+    return "";
+  } else {
+    name = name.split(" ");
+    if (name.length > 1) {
+      return nameSwitch(name[0], name[1]);
+    }
+  }
+  return name;
+};
 
 module.exports = nameInverter;
--- nameFormatter/test/nameInverterTest.js
+++ nameFormatter/test/nameInverterTest.js
@@ -5,45 +5,49 @@ const nameInverter = require("../nameInverter.js");
 
 describe("nameInverter", function () {
   it("return an empty string when passed an empty string", function () {
-    input: "";
-    output: "";
+    const inputName = "";
+    const expectedOutput = "";
+    assert.equal(nameInverter(inputName), expectedOutput);
   });
   it("return a single name when passed a single name", function () {
-    input: "name";
-    output: "name";
+    const inputName = "name";
+    const expectedOutput = "name";
+    assert.equal(nameInverter(inputName), expectedOutput);
   });
   it("return a single name when passed a single name with extra spaces", function () {
-    input: " name ";
-    output: "name";
+    const inputName = " name ";
+    const expectedOutput = "name";
+    assert.equal(nameInverter(inputName), expectedOutput);
   });
   it("return a last-name, first-name when passed a first and last-name", function () {
-    input: "first last";
-    output: "last, first";
+    const inputName = "first last";
+    const expectedOutput = "last, first";
+    assert.equal(nameInverter(inputName), expectedOutput);
   });
-  it("Stretch: return a last-name, first-name when passed a first and last-name with extra spaces around the names (We may want to use regular expressions to detect the whitespace)", function () {
-    input: " first last";
-    output: "last, first";
-  });
-  it("return an empty string when passed a single honorific", function () {
-    input: "Dr. ";
-    output: "";
-  });
-  it("return honorific first-name when passed honorific first-name", function () {
-    input: "Dr. first";
-    output: "Dr. first";
-  });
-  it("return a honorific last-name, first-name when passed honorific first-name last-name", function () {
-    input: "Dr. first-name last-name";
-    output: "Dr. last-name, first-name";
-  });
-  it("return a honorific last-name, first-name when passed honorific first-name last-name with extra spaces around the words", function () {
-    input: " Dr. first-name last-name ";
-    output: "Dr. last-name, first-name";
-  });
-  it("throw an error when name is undefined", function () {
-    input: undefined;
-    output: throw "Error";
-  });
+  // it("Stretch: return a last-name, first-name when passed a first and last-name with extra spaces around the names (We may want to use regular expressions to detect the whitespace)", function () {
+  //   input: " first last";
+  //   output: "last, first";
+  // });
+  // it("return an empty string when passed a single honorific", function () {
+  //   input: "Dr. ";
+  //   output: "";
+  // });
+  // it("return honorific first-name when passed honorific first-name", function () {
+  //   input: "Dr. first";
+  //   output: "Dr. first";
+  // });
+  // it("return a honorific last-name, first-name when passed honorific first-name last-name", function () {
+  //   input: "Dr. first-name last-name";
+  //   output: "Dr. last-name, first-name";
+  // });
+  // it("return a honorific last-name, first-name when passed honorific first-name last-name with extra spaces around the words", function () {
+  //   input: " Dr. first-name last-name ";
+  //   output: "Dr. last-name, first-name";
+  // });
+  // it("throw an error when name is undefined", function () {
+  //   input: undefined;
+  //   output: throw "Error";
+  // });
 });
 
 /*
