e1e197758b31a63c3d3e303d58e328ee03011352
James Murphy <jamesm8228@gmail.com>
2023-02-28 14:03:06 -0500

    add commentation to make file question and answer more clear

--- kataCodeWars.js
+++ kataCodeWars.js
@@ -159,6 +159,9 @@ function countBy(x, n) {
   return z;
 }
 
+
+//Practice 7 
+
 // Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
 // For example,
