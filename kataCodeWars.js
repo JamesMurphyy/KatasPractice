696e2d9e94062a56d04475e4a5c31d4b24c2563a
James Murphy <jamesm8228@gmail.com>
2023-03-07 11:02:26 -0500

    Add another question

--- kataCodeWars.js
+++ kataCodeWars.js
@@ -280,3 +280,40 @@ function repeatStr(n, s) {
 function keepOrder(ary, val) {
   return ary.filter(x => x < val).length;
 }
+
+// Practice 11
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+function betterThanAverage(classPoints, yourPoints) {
+  // Your code here
+  let totalSumScores = 0
+  let averageScore = 0
+  let numberOfScores = 0
+
+  classPoints.push(yourPoints)
+
+  classPoints.forEach(x => {
+    totalSumScores += x
+    numberOfScores++
+  })
+
+  averageScore = (totalSumScores / numberOfScores)
+
+  return ((yourPoints > averageScore) ? true : false)
+
+}
+
+// OR
+
+function betterThanAverage(classPoints, yourPoints) {
+  return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
+}
