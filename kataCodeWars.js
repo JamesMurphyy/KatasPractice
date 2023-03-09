12e0adf9964a0a4453d58c941f362520ea44ea7b
James Murphy <jamesm8228@gmail.com>
2023-03-09 10:39:49 -0500

    add question

--- kataCodeWars.js
+++ kataCodeWars.js
@@ -293,6 +293,8 @@ function keepOrder(ary, val) {
 // Note:
 // Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
 
+
+//ANSWER
 function betterThanAverage(classPoints, yourPoints) {
   // Your code here
   let totalSumScores = 0
@@ -316,4 +318,22 @@ function betterThanAverage(classPoints, yourPoints) {
 
 function betterThanAverage(classPoints, yourPoints) {
   return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length;
+}
+
+// Practice 12
+
+// Classy Extensions
+// Classy Extensions, this kata is mainly aimed at the new JS ES6 Update introducing extends keyword. You will be preloaded with the Animal class, so you should only edit the Cat class.
+// Task
+// Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+// The name attribute is passed with this.name (JS), @name (Ruby) or self.name (Python).
+
+// Reference: JS, Ruby, Python.
+
+//ANSWER
+
+class Cat extends Animal {
+  speak() {
+    return `${this.name} meows.`
+  }
 }
