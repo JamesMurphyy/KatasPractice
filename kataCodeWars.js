8ac224b0d54bb3eab8486edd500050fa2b025acc
James Murphy <jamesm8228@gmail.com>
2023-03-13 10:34:47 -0400

    add questions

--- kataCodeWars.js
+++ kataCodeWars.js
@@ -398,3 +398,38 @@ function sumTwoSmallestNumbers(numbers) {
   })
   return (sorted[0] + sorted[1])
 }
+
+// Practice 15
+
+// Your task is to create a change machine.
+
+// The machine accepts a range of specified coins and notes, it returns change in 20p and 10p coins in the minimum amount of pieces. A 50p for example would return two 20p pieces and one 10p piece. The machine will always try and return change, if you input a 20p for example it will return "10p 10p".
+
+// The machine accepts these coins and notes: £5, £2, £1, 50p, 20p. Any coins and notes which are not accepted by the machine will be returned. If you were to put a £20 note into the machine for example, it would be returned to you and not broken into change.
+
+// This change machine is programmed to accept and distribute strings rather than numbers. The change will be returned as one string with the change separated by single spaces & no commas. The values of the string will be descending.
+
+//ANSWER
+
+function changeMe(moneyIn){
+	switch (moneyIn) {
+  	case '£5': return '20p '.repeat(25).trim()
+  	case '£2': return '20p '.repeat(10).trim()
+  	case '£1': return '20p '.repeat(5).trim()
+  	case '50p': return '20p 20p 10p'
+  	case '20p': return '10p 10p'
+    default: return moneyIn
+  }
+}
+
+// Practice 16
+
+// It is easy to join two strings together like this string1 + string2.
+
+// Another way to get the desired result would be with string1.concat(string2)
+
+// ES6 has introduced another way of joining strings. Your task is to find out what this is and write a function that will add two strings together, there must be a space between the two strings.
+
+// + , .concat() & .join() will not be allowed in this exercise. I have also removed some other methods that can be used to cheat!
+
+// If one of the arguments is a number your code must coerce it into being a string.
