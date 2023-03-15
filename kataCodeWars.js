f5f1d255bf37f0a2782dfa4422154b02e52e11c5
James Murphy <jamesm8228@gmail.com>
2023-03-15 11:46:20 -0400

    Add question

--- kataCodeWars.js
+++ kataCodeWars.js
@@ -474,3 +474,23 @@ function periodIsLate(last, today, cycleLength) {
 function reverseWords(str) {
   return str.split("").reverse().join("").split(" ").reverse().join(" ")
 };
+
+
+
+// Practice 19
+
+// Your task is to find the nearest square number, nearest_sq(n) or nearestSq(n), of a positive integer n.
+
+// For example, if n = 111, then nearest\_sq(n) (nearestSq(n)) equals 121, since 111 is closer to 121, the square of 11, than 100, the square of 10.
+
+// If the n is already the perfect square (e.g. n = 144, n = 81, etc.), you need to just return n.
+
+// Good luck :)
+
+//ANSWER
+
+function nearestSq(n) {
+  let i, j;
+  for (i = j = n; Math.sqrt(i) % 1 != 0 && Math.sqrt(j) % 1 != 0; i++, j--);
+  return Math.sqrt(i) % 1 == 0 ? i : j
+}
