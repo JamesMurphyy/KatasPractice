f95243eda8a1c975ac0b09f55fd3b5838a397f8f
James Murphy <jamesm8228@gmail.com>
2023-03-21 10:47:31 -0400

    Add question

--- kataCodeWars.js
+++ kataCodeWars.js
@@ -787,3 +787,37 @@ function openOrSenior(data) {
   }
   return ans;
 }
+
+// Practice 32
+
+// Task:
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+// ANSWER
+
+function SeriesSum(n) {
+  let ans = 0;
+  let num = 1;
+  for (let i = 0; i < n; i += 1) {
+    if (i === 0) {
+      ans = 1;
+    } else {
+      num += 3;
+      ans += (1 / num);
+    }
+  }
+  return ans.toFixed(2);
+};
