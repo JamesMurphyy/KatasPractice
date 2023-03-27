22c363091255314632a1e46b5a0dff394021e4ba
James Murphy <jamesm8228@gmail.com>
2023-03-27 10:57:46 -0400

    Add question

--- kataCodeWars.js
+++ kataCodeWars.js
@@ -986,3 +986,62 @@ function fakeBin(x) {
 function fakeBin(x) {
   return x.replace(/\d/g, d => d < 5 ? 0 : 1);
 }
+
+
+// Practice 41
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+// Note: keep the original order of the names in the output.
+
+// ANSWER
+
+function friend(friends) {
+  //your code here
+  return friends.filter((x) => x.length === 4)
+}
+
+// Practice 42
+
+// Grade book
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+// ANSWER
+
+function getGrade(s1, s2, s3) {
+  // Code here
+  let avgSum = (s1 + s2 + s3) / 3
+  if (avgSum >= 90) {
+    return 'A'
+  } else if (avgSum >= 80) {
+    return 'B'
+  } else if (avgSum >= 70) {
+    return 'C'
+  } else if (avgSum >= 60) {
+    return 'D'
+  } else {
+    return 'F'
+  }
+}
+
+// OR
+
+function getGrade(s1, s2, s3) {
+  var s = (s1 + s2 + s3) / 3
+  return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F"
+}
