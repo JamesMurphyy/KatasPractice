facccacf3f8b0a576207a0a8be3704ca97245af7
James Murphy <jamesm8228@gmail.com>
2023-03-28 10:48:09 -0400

    Add question

--- kataCodeWars.js
+++ kataCodeWars.js
@@ -1070,3 +1070,29 @@ function isPangram(string) {
 
   return alphabet.join('') === result.sort().join('');
 }
+
+
+// Practice 44
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+//ANSWER
+
+function past(h, m, s) {
+  //#Happy Coding! ^_^
+  return h * 3600000 + m * 60000 + s * 1000;
+}
