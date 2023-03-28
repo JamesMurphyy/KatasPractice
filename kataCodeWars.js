719a8201baba68d242c0861a41e7983d9ac68ba3
James Murphy <jamesm8228@gmail.com>
2023-03-28 11:11:48 -0400

    Add question

--- kataCodeWars.js
+++ kataCodeWars.js
@@ -1096,3 +1096,28 @@ function past(h, m, s) {
   //#Happy Coding! ^_^
   return h * 3600000 + m * 60000 + s * 1000;
 }
+
+// Practice 45
+
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+//ANSWER
+
+function countPositivesSumNegatives(input) {
+  // your code here
+  let positiveSum = 0
+  let negativeSum = 0
+  if (input === null || input.length === 0) {
+    return [];
+  } else {
+    input.forEach((num) => num > 0 ? positiveSum++ : negativeSum += num)
+  }
+  return [positiveSum, negativeSum]
+}
