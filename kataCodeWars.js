ee53989912b6174780b19eb396f791c3f875d370
James Murphy <jamesm8228@gmail.com>
2023-03-29 10:41:30 -0400

    Add question

--- kataCodeWars.js
+++ kataCodeWars.js
@@ -1154,6 +1154,39 @@ function DNAtoRNA(dna) {
 
 //OR
 
-function DNAtoRNA(dna){
+function DNAtoRNA(dna) {
   return dna.replace(/T/g, 'U');
+}
+
+
+// Practice 47
+
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+//ANSWER
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let smallestInt = args[0]
+    for (let arg of args) {
+      if (arg < smallestInt) {
+        smallestInt = arg
+      }
+    }
+    return smallestInt
+  }
+}
+
+//OR
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args)
+  }
 }
