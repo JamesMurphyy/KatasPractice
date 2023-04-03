d94eae54954d194e9e67a29f394056590d5e58aa
James Murphy <jamesm8228@gmail.com>
2023-04-03 10:39:04 -0400

    Add question

--- kataCodeWars.js
+++ kataCodeWars.js
@@ -1217,3 +1217,30 @@ function insertDash(num) {
   if (ans[ans.length - 1] == '-') ans.pop()
   return ans.join("")
 }
+
+// Practice 48
+
+// Given an array of numbers, return the difference between the largest and smallest values.
+
+// For example:
+
+// [23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
+
+// [1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
+
+// The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
+
+//ANSWER
+
+function betweenExtremes(numbers) {
+  //write your code here
+  let max = Math.max(...numbers)
+  let min = Math.min(...numbers)
+  return (max - min)
+}
+
+//OR
+
+function betweenExtremes(numbers) {
+  return Math.max(...numbers) - Math.min(...numbers);
+}
