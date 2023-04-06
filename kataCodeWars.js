f8b61234d46e9f80cea84223d390839e8f00f66e
James Murphy <jamesm8228@gmail.com>
2023-04-06 10:49:41 -0400

    Add question

--- kataCodeWars.js
+++ kataCodeWars.js
@@ -1300,10 +1300,10 @@ function evenOrOdd(number) {
 
 //ANSWER
 
-var uniqueInOrder = function (iterable) {
+const uniqueInOrder = function (iterable) {
   //your code here - remember iterable can be a string or an array
-  var res = [];
-  for (var i = 0; i < iterable.length; i++) {
+  let res = [];
+  for (let i = 0; i < iterable.length; i++) {
     if (iterable[i] != iterable[i + 1]) res.push(iterable[i]);
   }
   return res;
