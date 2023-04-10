84fc33e71c42c983cbbc5d69ea9c089c74c56d99
James Murphy <jamesm8228@gmail.com>
2023-04-10 17:07:52 -0400

    Add leetcode questions

--- kataLeetCode.js
+++ kataLeetCode.js
@@ -128,8 +128,6 @@ var removeDuplicates = function (nums) {
 
 // If there is no common prefix, return an empty string "".
 
-
-
 //   Example 1:
 
 // Input: strs = ["flower", "flow", "flight"]
@@ -160,4 +158,44 @@ var longestCommonPrefix = function (strs) {
     prevalues = values;
   }
   return values;
+};
+
+
+// Question 5
+
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+// The overall run time complexity should be O(log (m+n)).
+
+
+
+// Example 1:
+
+// Input: nums1 = [1,3], nums2 = [2]
+// Output: 2.00000
+// Explanation: merged array = [1,2,3] and median is 2.
+// Example 2:
+
+// Input: nums1 = [1,2], nums2 = [3,4]
+// Output: 2.50000
+// Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+
+// ANSWER
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+
+var findMedianSortedArrays = function (nums1, nums2) {
+  let concatArray = nums1.concat(nums2)
+  let sortedArray = (concatArray.sort((a, b) => (a - b)))
+  if (sortedArray.length % 2 === 0) {
+    let firstVal = sortedArray[Math.floor(sortedArray.length / 2) - 1]
+    let secondVal = sortedArray[Math.floor(sortedArray.length / 2)]
+    return ((firstVal + secondVal) / 2)
+  }
+  return Number(sortedArray[Math.floor(sortedArray.length / 2)])
 };
