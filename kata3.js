4a620ab6216bdd151e604ec1f4008e481066b438
James Murphy <jamesm8228@gmail.com>
2023-04-10 17:14:10 -0400

    add code refactor

--- kata3.js
+++ kata3.js
@@ -1,15 +1,28 @@
-const numberOfVowels = function(data) {
-  vowels = ['a', 'e', 'i', 'o', 'u']
-  ans = []
+// const numberOfVowels = function(data) {
+//   vowels = ['a', 'e', 'i', 'o', 'u']
+//   ans = []
+//   for (let i = 0; i < data.length; i++) {
+//     console.log(data[i])
+//     for (let j = 0; j<vowels.length; j++)
+//       if (data[i] === vowels[j]) {
+//         ans.push(data[i])
+//       }
+
+//   }  
+//   return ans.length 
+// };
+
+const numberOfVowels = function (data) {
+  let vowels = ['a', 'e', 'i', 'o', 'u']
+  let ans = []
   for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j<vowels.length; j++)
-      if (data[i] === vowels[j]) {
-        ans.push(data[i])
-      }
-  
-  }  
-  return ans.length 
+    if (vowels.includes(data[i])) ans.push(data[i])
+  }
+  return ans.length
 };
+
+
+
 
 console.log(numberOfVowels("orange"));
 console.log(numberOfVowels("lighthouse labs"));
