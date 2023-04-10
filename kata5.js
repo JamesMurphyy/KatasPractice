2c4be3bb1f62f479ec4b2f9000808ba995e2b6e9
James Murphy <jamesm8228@gmail.com>
2023-04-10 17:24:55 -0400

    Add refactor changes

--- kata5.js
+++ kata5.js
@@ -1,20 +1,24 @@
 
+// const urlEncode = function(text) {
+//   let trimmedText = text.trim();  
+//   let ans = "";
+//   let space = "%20";
+ 
+// for (let i = 0; i < trimmedText.length; i++){
+//   if (trimmedText[i] === " "){
+//     ans = ans + space;
+//   } else {
+//     ans = ans + trimmedText[i];
+//   }
+// }
+//   return ans;
+// };
+
 const urlEncode = function(text) {
   let trimmedText = text.trim();  
-  let ans = "";
-  let space = "%20";
- 
-for (let i = 0; i < trimmedText.length; i++){
-  if (trimmedText[i] === " "){
-    ans = ans + space;
-  } else {
-    ans = ans + trimmedText[i];
-  }
-}
-  return ans;
+  return trimmedText.replace(/\s+/g, "%20")
 };
-
 
 console.log(urlEncode("Lighthouse Labs"));
 console.log(urlEncode("            Lighthouse Labs "));
-console.log(urlEncode("blue is greenergefdsadf than purple for sure"));
+console.log(urlEncode("blue is  greenergefdsadf than purple for sure"));
