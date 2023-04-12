0ecfce3293049607d59c0b3d724a16ee17cc0c07
James Murphy <jamesm8228@gmail.com>
2023-04-12 15:12:55 -0400

    Add refactor for question

--- kata13Grad.js
+++ kata13Grad.js
@@ -1,21 +1,21 @@
 
-const talkingCalendar = function(date) {
+const talkingCalendar = function (date) {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   let arr = date.split("/")
   let year = arr[0]
-  let month = months[arr[1]-1]
+  let month = months[arr[1] - 1]
   let day = arr[2]
 
-  if (day.substring(0,1) === '0') {
+  if (day.substring(0, 1) === '0') {
     day = day.substring(1)
   }
-  
+
   if (day === '1' || day === '21' || day === '31') {
-    day  += 'st'
+    day += 'st'
   } else if (day === '2' || day === '22') {
-    day  += 'nd'
+    day += 'nd'
   } else if (day === '3' || day === '23') {
-    day  += 'rd'
+    day += 'rd'
   } else {
     day += 'th'
   }
