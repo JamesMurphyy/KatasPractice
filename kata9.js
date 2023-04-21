30d9d6f998ab9482775664dacd7a5f0080d0ae3d
James Murphy <jamesm8228@gmail.com>
2023-04-21 12:30:57 -0400

    Answer refactor

--- kata9.js
+++ kata9.js
@@ -1,15 +1,15 @@
-const camelCase = function(input) {
+const camelCase = function (input) {
   let ans = ""
   for (let i = 0; i < input.length; i++) {
-    
+
     if (input[i] !== " ") {
       ans += input[i]
     } else if (input[i] === " ") {
-      ans += input[i+1].toUpperCase()
-      i ++
+      ans += input[i + 1].toUpperCase()
+      i++
     }
-};
-return ans
+  };
+  return ans
 }
 console.log(camelCase("gggggggggggggg"));
 console.log(camelCase("loopy lighthouse"));
