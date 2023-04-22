2d800168b8201d23657766c0495bcb6084815f87
James Murphy <jamesm8228@gmail.com>
2023-04-22 10:14:47 -0400

    Add refactor changes

--- kata8.js
+++ kata8.js
@@ -1,12 +1,12 @@
-const repeatNumbers = function(data) {
+const repeatNumbers = function (data) {
   return (Array.isArray(data[0]) ? data : [data])
-      .map(([value, length]) => Array.from({ length }, _ => value).join(''))
-      .join(', ');
+    .map(([value, length]) => Array.from({ length }, _ => value).join(''))
+    .join(', ');
 }
 
 
 
 console.log(repeatNumbers("42, 7"));
-console.log(repeatNumbers([[10, 2], [11,1]]));
+console.log(repeatNumbers([[10, 2], [11, 1]]));
 console.log(repeatNumbers([[1, 2], [2, 3]]));
 console.log(repeatNumbers([[1, 4], [3, 6], [9, 2]]));
