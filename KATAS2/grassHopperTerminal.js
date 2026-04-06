01b83e815488871b167f8067e2b040d61f32ca14
James Murphy <jamesm8228@gmail.com>
2026-04-06 09:45:49 -0400

    Add Kata practice

---
+++ KATAS2/grassHopperTerminal.js
@@ -0,0 +1,16 @@
+/*
+Terminal game move function
+In this game, the hero moves from left to right. The player rolls the die and moves the number of spaces indicated by the die two times.
+
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+Example:
+move(3, 6) should equal 15
+*/
+
+function move(position, roll) {
+  // return the new position
+  return Number(position + roll * 2);
+
+  // Dont need to add the "Number() function -- can use 'return position + roll * 2' "
+}
